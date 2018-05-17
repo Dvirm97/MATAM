0eb973579cb67b9c092e68062113b5b0c91a35d9
Dvirm97 <38911355+Dvirm97@users.noreply.github.com>
2018-05-17 12:35:39 +0300

    Update series.h

--- hw3/series.h
+++ hw3/series.h
@@ -1,8 +1,14 @@
-//
-// Created by shani on 15/05/2018.
-//
 
 #ifndef HW3_SERIES_H
 #define HW3_SERIES_H
+#include "map.h"
+
+typedef struct series* Series;
+
+MapDataElement copySeries(MapDataElement src);
+MapKeyElement seriesCopyName(MapKeyElement src);
+void deleteSeries(MapDataElement user);
+void seriesDeleteName(MapKeyElement name);
+int seriesCompareNames(MapKeyElement name1, MapKeyElement name2);
 
 #endif //HW3_SERIES_H
