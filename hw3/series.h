db6f39d97fbea516c3657ca0cbae5a73984fc0f7
Dvirm97 <38911355+Dvirm97@users.noreply.github.com>
2018-05-18 03:18:37 +0300

    Update series.h

--- hw3/series.h
+++ hw3/series.h
@@ -2,6 +2,7 @@
 #ifndef HW3_SERIES_H
 #define HW3_SERIES_H
 #include "map.h"
+#include "mtm_ex3.h"
 
 typedef struct series* Series;
 
@@ -10,5 +11,10 @@ MapKeyElement seriesCopyName(MapKeyElement src);
 void deleteSeries(MapDataElement user);
 void seriesDeleteName(MapKeyElement name);
 int seriesCompareNames(MapKeyElement name1, MapKeyElement name2);
-
+Series createSeries(const char* name, int episodesNum,
+                    Genre genre, int* ages, int episodesDuration);
+int* seriesGetAges(Series series);
+/*const char* genres[] = { "SCIENCE_FICTION", "DRAMA", "COMEDY", "CRIME",
+                           "MYSTERY", "DOCUMENTARY", "ROMANCE", "HORROR"};
+ */
 #endif //HW3_SERIES_H
