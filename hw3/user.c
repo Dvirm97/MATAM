0911853f0e2bdbfdecf226f4ecff9c40e048dfad
Dvirm97 <38911355+Dvirm97@users.noreply.github.com>
2018-05-18 03:16:03 +0300

    Update user.c

--- hw3/user.c
+++ hw3/user.c
@@ -4,9 +4,16 @@
 #include <stdio.h>
 #include <string.h>
 #include "map.h"
+#include "set.h"
+#include "series.h"
+#include "mtm_ex3.h"
+
+
 struct user {
     char* username;
     int age;
+    Set favorites;
+    Set friends;
 };
 
 MapDataElement copyUser(MapDataElement src) {
@@ -20,6 +27,8 @@ MapDataElement copyUser(MapDataElement src) {
     User res = (User)src; //doing this right?
     strcpy(dst->username, res->username); //dunno if need another malloc for this
     dst->age = res->age;
+    dst->favorites = setCopy(res->favorites);
+    dst->friends = setCopy(res->friends);
     return dst;
 }
 MapKeyElement userCopyName(MapKeyElement src) {
@@ -38,11 +47,35 @@ void userDeleteName(MapKeyElement name) {
 int userCompareNames(MapKeyElement name1, MapKeyElement name2) {
     return strcmp((char*)name1, (char*)name2);
 }
-User createUser(char* newUsername, int newAge) {
+User createUser(const char* newUsername, int newAge) {
     User user = malloc(sizeof(user));
     strcpy(user->username, newUsername);
     user->age = newAge;
+    user->favorites = setCreate(copySeries, deleteSeries, seriesCompareNames);
+    user->friends = setCreate(copyUser, deleteUser, userCompareNames);
     return user;
+}
+//I guess the next function should be USER_STATUS (which we should implement), maybe later
+MtmFlixResult userAddFavorite(User user, Series series) {
+    if (!user || !series)
+        return MTMFLIX_NULL_ARGUMENT;
+    //checking if user age is in series age range
+    //(should be defined separately in a static function)
+    //(should probably make this check in mtmflix.c and not here):
+    int* seriesAges = seriesGetAges(series);
+    if (seriesAges)
+        if (user->age < *seriesAges || user->age > *(seriesAges + 1))
+            return MTMFLIX_USER_NOT_IN_THE_RIGHT_AGE;
+
+    if (setAdd(user->favorites, series) == SET_OUT_OF_MEMORY)
+        return MTMFLIX_OUT_OF_MEMORY;
+    return MTMFLIX_SUCCESS;
+}
+MtmFlixResult userRemoveFavorite(User user, Series series) {
+    if (!user || !series)
+        return MTMFLIX_NULL_ARGUMENT;
+    setRemove(user->favorites, series);
+    return MTMFLIX_SUCCESS;
 }
 void printUser(User user) { //remove this function
     printf("%s\n%d", user->username, user->age);
