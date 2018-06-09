78a4d8644d82f9719b44a9f09941093a9421df77
Dvirm97 <38911355+Dvirm97@users.noreply.github.com>
2018-06-09 18:05:00 +0300

    Update game.h

--- game.h
+++ game.h
@@ -1,14 +1,29 @@
-//
-// Created by user on 09-Jun-18.
-//
+#include "Player.h"
+#include "Weapon.h"
 
 #ifndef HW4_GAME_H
 #define HW4_GAME_H
 
 
-class game {
-
+class Game {
+    int maxPlayers;
+    Player* playersArr[];
+public:
+    explicit Game(int maxPlayers);
+    ~Game();
 };
+
+Game::Game(int maxPlayers) {
+    this->maxPlayers = maxPlayers;
+    this->playersArr = new Player*[maxPlayers];
+}
+Game::~Game() {
+    for (int i=0; i < maxPlayers; i++) {
+        if (playersArr[i])
+            delete *(playersArr[i]);
+    }
+    delete[] playersArr;
+}
 
 
 #endif //HW4_GAME_H
